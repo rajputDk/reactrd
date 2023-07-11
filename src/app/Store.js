153bad7ce17da13883bdb8a3f80ec2bd9f4fc0d1
Deepak <dhonideepaksingh123@gmail.com>
2023-07-12 00:22:56 +0530

    redux

---
+++ src/app/Store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import CounterReducer from '../features/counter/CounterSlice'
+
+
+export const store = configureStore({
+  reducer: {
+    counter: CounterReducer,
+  },
+})
